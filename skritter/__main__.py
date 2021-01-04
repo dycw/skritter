371aba9cd8f5a41defb2d5e300a43014187c6f41
Derek Wan <d.wan@icloud.com>
2021-01-04 13:17:55 +0800

    Initial commit (#1)

---
+++ skritter/__main__.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+
+from time import sleep
+
+from click import command
+from click import option
+from pynput.keyboard import Controller
+from pynput.keyboard import Key
+from tqdm import tqdm
+
+
+KEYBOARD = Controller()
+INIT_STEP = 0.1
+
+
+def _make_tqdm(*, total: int, step: float, desc: str) -> tqdm:
+    return tqdm(range(int(total / step)), desc=desc)
+
+
+@command()
+@option("--init", default=5, type=int)
+@option("--duration", default=60 * 60, type=int)
+@option("--step", default=1.0, type=float)
+def main(
+    *,
+    init: int,
+    duration: int,
+    step: float,
+) -> None:
+    for _ in _make_tqdm(
+        total=init,
+        step=INIT_STEP,
+        desc="Initializing",
+    ):
+        sleep(INIT_STEP)
+
+    for _ in _make_tqdm(
+        total=duration,
+        step=step,
+        desc="Running",
+    ):
+        KEYBOARD.press(Key.enter)
+        KEYBOARD.release(Key.enter)
+        sleep(step)
+
+
+if __name__ == "__main__":
+    main()
