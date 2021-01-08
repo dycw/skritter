2dcca19d04470f5e3c5c63ada88227772bd93c57
Derek Wan <d.wan@icloud.com>
2021-01-09 07:54:44 +0800

    Restore init=5

--- skritter/__main__.py
+++ skritter/__main__.py
@@ -16,7 +16,7 @@ def _make_tqdm(*, total: int, step: float, desc: str) -> tqdm:
 
 
 @command()
-@option("--init", default=10, type=int)
+@option("--init", default=5, type=int)
 @option("--duration", default=60 * 60, type=int)
 @option("--step", default=3.0, type=float)
 def main(
