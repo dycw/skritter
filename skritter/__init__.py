a2a9040e63eb625fe2c2383ed8ffe00af6739166
Derek Wan <d.wan@icloud.com>
2021-01-09 08:55:41 +0800

    Correctly collate mouse events

--- skritter/__init__.py
+++ skritter/__init__.py
@@ -1,1 +1,64 @@
+from contextlib import contextmanager
+from itertools import cycle
+from logging import basicConfig
+from logging import getLogger
+from logging import INFO
+from sys import stdout
+from time import sleep
+from timeit import default_timer
+from typing import Iterator
+from typing import List
+
+from pynput.keyboard import Controller
+from pynput.keyboard import Key
+from pynput.mouse import Events
+
+
+basicConfig(
+    datefmt="%Y-%m-%d %H:%M:%S",
+    format="{asctime}: {msg}",
+    level=INFO,
+    stream=stdout,
+    style="{",
+)
+LOGGER = getLogger(__name__)
+
+
+@contextmanager
+def collect_mouse_events(duration: float) -> Iterator[List[Events]]:
+    start = default_timer()
+    end = start + duration
+    events: List[Events] = []
+    while (dur := end - default_timer()) > 0.0:
+        with Events() as evs:
+            if (ev := evs.get(timeout=dur)) is not None:
+                events.append(ev)
+    yield events
+
+
+def loop_until_click(
+    *,
+    init: int,
+    test: int,
+    review: int,
+) -> None:
+    LOGGER.info("Initializing... (please go to https://skritter.com/study)")
+    sleep(init)
+
+    LOGGER.info("Running...")
+    keyboard = Controller()
+    for key, dur in cycle([(Key.enter, test), ("3", review)]):
+        LOGGER.info("start")
+        with collect_mouse_events(dur) as events:
+            if any(isinstance(event, Events.Click) for event in events):
+                LOGGER.info("Aborting")
+                return
+            else:
+                LOGGER.info(f"Pressing {key}...")
+                keyboard.press(key)
+                keyboard.release(key)
+                LOGGER.info(f"Sleeping {dur}...")
+        LOGGER.info("end")
+
+
 __version__ = "0.0.3"
--- skritter/__main__.py
+++ skritter/__main__.py
@@ -1,49 +1,20 @@
-from itertools import cycle
-from logging import basicConfig
-from logging import getLogger
-from logging import INFO
-from sys import stdout
-from time import sleep
-
 from click import command
 from click import option
-from pynput.keyboard import Controller
-from pynput.keyboard import Key
-from pynput.mouse import Events
 
-
-basicConfig(
-    datefmt="%Y-%m-%d %H:%M:%S",
-    format="{asctime}: {msg}",
-    level=INFO,
-    stream=stdout,
-    style="{",
-)
-LOGGER = getLogger(__file__)
-LOGGER.setLevel(INFO)
-KEYBOARD = Controller()
+from skritter import loop_until_click
 
 
 @command()
 @option("--init", default=5, type=int)
-@option("--step", default=3.0, type=float)
+@option("--test", default=4, type=int)
+@option("--review", default=2, type=int)
 def main(
     *,
     init: int,
-    step: float,
+    test: int,
+    review: int,
 ) -> None:
-    LOGGER.info("Initializing...")
-    sleep(init)
-
-    LOGGER.info("Running...")
-    for key in cycle([Key.enter, "3"]):
-        LOGGER.info("Waiting for events")
-        with Events() as events:
-            if isinstance(events.get(timeout=step), Events.Click):
-                LOGGER.info("Stopping")
-                return
-        KEYBOARD.press(key)
-        KEYBOARD.release(key)
+    loop_until_click(init=init, test=test, review=review)
 
 
 if __name__ == "__main__":
