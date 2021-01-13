37a6d3c606a30e94ff7dec4b5cc3ed61474a1a81
Derek Wan <d.wan@icloud.com>
2021-01-13 11:15:09 +0800

    Switch to keyboard based controls, can forget current or previous  (#12)
    * Switch to keyboard based controls, allowing forget current/previous
    
    * Continue improving script
    
    * Use padded descriptions
    
    * Use some neat Enums
    
    * Reset defaults again
    
    * Bump version: 0.0.11 → 0.1.0
    
    * Bump forgotten to 1.5
    
    * Even longer forgotten

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ from logging import INFO
 from sys import stdout
 from time import sleep
 from timeit import default_timer
-from typing import Any
+from typing import Iterator
 
 from click import command
 from click import option
+from more_itertools import peekable
 from pynput.keyboard import Controller
+from pynput.keyboard import Events
 from pynput.keyboard import Key
-from pynput.mouse import Events
 from tqdm import tqdm
 
 
@@ -25,53 +26,25 @@ basicConfig(
     stream=stdout,
     style="{",
 )
+
+
 DEFAULT_INIT = 2.0
-DEFAULT_TEST = 1.25
-DEFAULT_REVIEW = 2.0
-DEFAULT_REVIEW_FORGOTTEN = 1.0
-LOGGER = getLogger(__name__)
+DEFAULT_TEST = 1.5
+DEFAULT_REVIEW = 1.5
+DEFAULT_FORGOTTEN = 3.0
 
 
-def loop(
-    *,
-    init: float,
-    test: float,
-    review: float,
-    review_forgotten: float,
-) -> None:
-    LOGGER.info("Initializing... (please go to https://skritter.com/study)")
-    step = 0.01
-    for _ in tqdm(range(int(init / step))):
-        sleep(step)
+_CONTROLLER = Controller()
+_LOGGER = getLogger(__name__)
+_TQDM_STEP = 0.1
 
-    LOGGER.info("Running...")
-    press_3()
-    skip_review = False
-    for phase in cycle(Phase):
-        LOGGER.info(phase)
-        if phase is Phase.test:
-            if (clicks := count_clicks(duration=test, max_clicks=2)) == 0:
-                press_enter()
-            elif clicks == 1:
-                log_forgotten()
-                press_enter()
-                press_1_and_sleep(review_forgotten)
-                skip_review |= True
-            else:
-                return log_end()
-        elif phase is Phase.review:
-            if skip_review:
-                skip_review &= False
-            else:
-                if (clicks := count_clicks(duration=review, max_clicks=2)) == 0:
-                    press_3()
-                elif clicks == 1:
-                    log_forgotten()
-                    press_1_and_sleep(review_forgotten)
-                else:
-                    return log_end()
-        else:
-            raise ValueError(f"Invalid phase: {phase}")
+
+class Forgotten(Enum):
+    current = auto()
+    previous = auto()
+
+    def __str__(self) -> str:
+        return f"Marking {self.name} as forgotten..."
 
 
 class Phase(Enum):
@@ -79,69 +52,114 @@ class Phase(Enum):
     review = auto()
 
 
-def log_end() -> None:
-    LOGGER.info("Ending session...")
+class Status(Enum):
+    success = auto()
+    fail_current = auto()
+    fail_previous = auto()
+    terminate = auto()
 
 
-def log_forgotten() -> None:
-    LOGGER.info("Marking as forgotten...")
+class TqdmDesc(Enum):
+    initial = auto()
+    test = auto()
+    review = auto()
+    confirm = auto()
 
+    def __str__(self) -> str:
+        max_len = max(len(desc.ing) for desc in TqdmDesc)
+        template = f"{{:{max_len}}}"
+        return template.format(self.ing)
 
-def press(key: Any) -> None:
-    keyboard = Controller()
-    keyboard.press(key)
-    keyboard.release(key)
-
-
-def press_enter() -> None:
-    press(Key.enter)
-
-
-def press_1() -> None:
-    press("1")
-
-
-def press_1_and_sleep(review: float) -> None:
-    press_1()
-    press(Key.left)
-    sleep(review)
-    press_1()
-
-
-def press_3() -> None:
-    press("3")
-
-
-def count_clicks(duration: float, max_clicks: int) -> int:
-    start = default_timer()
-    end = start + duration
-    clicks = 0
-    while ((dur := end - default_timer()) > 0.0) and (clicks < max_clicks):
-        with Events() as events:
-            event = events.get(timeout=dur)
-            if isinstance(event, Events.Click) and event.pressed:
-                clicks += 1
-    return clicks
+    @property
+    def ing(self) -> str:
+        return self.name.title() + "ing"
 
 
 @command()
 @option("--init", default=DEFAULT_INIT, type=float)
 @option("--test", default=DEFAULT_TEST, type=float)
 @option("--review", default=DEFAULT_REVIEW, type=float)
-@option("--review-forgotten", default=DEFAULT_REVIEW_FORGOTTEN, type=float)
+@option("--forgotten", default=DEFAULT_FORGOTTEN, type=float)
 def main(
     *,
     init: float,
     test: float,
     review: float,
-    review_forgotten: float,
+    forgotten: float,
 ) -> None:
-    loop(
-        init=init,
-        test=test,
-        review=review,
-        review_forgotten=review_forgotten,
-    )
+    tqdm_sleep(dur=init, desc=TqdmDesc.initial)
+    phases = peekable(cycle(Phase))
+    for phase in phases:
+        if phase is Phase.test:
+            status = get_status(dur=test, desc=TqdmDesc.test)
+        elif phase is Phase.review:
+            status = get_status(dur=review, desc=TqdmDesc.review)
+        else:
+            raise ValueError(f"Invalid phase: {phase}")
+        if (phase is Phase.test) and (status is Status.success):
+            _CONTROLLER.tap("3")
+            _CONTROLLER.tap(Key.enter)
+        elif (phase is Phase.test) and (status is Status.fail_current):
+            _LOGGER.info(str(Forgotten.current))
+            _CONTROLLER.tap(Key.enter)
+            _CONTROLLER.tap("1")
+            fail_previous(forgotten, phases)
+        elif (phase is Phase.review) and (status is Status.success):
+            _CONTROLLER.tap("3")
+        elif (phase is Phase.review) and (status is Status.fail_current):
+            _LOGGER.info("Marking current as forgotten...")
+            fail_current(forgotten=forgotten, phases=phases)
+        elif status is Status.fail_previous:
+            _LOGGER.info(str(Forgotten.previous))
+            fail_previous(forgotten=forgotten, phases=phases)
+        elif status is Status.terminate:
+            _LOGGER.info("Terminating program...")
+            return
+        else:
+            raise ValueError(f"Invalid phase/status: {phase}, {status}")
+
+
+def tqdm_sleep(dur: float, desc: "TqdmDesc") -> None:
+    for _ in tqdm_dur(dur=dur, desc=desc):
+        sleep(_TQDM_STEP)
+
+
+def tqdm_dur(dur: float, desc: "TqdmDesc") -> Iterator[None]:
+    for _ in tqdm(range(int(dur / _TQDM_STEP)), desc=str(desc)):
+        yield None
+
+
+def get_status(dur: float, desc: "TqdmDesc") -> "Status":
+    for _ in tqdm_dur(dur=dur, desc=desc):
+        end = default_timer() + _TQDM_STEP
+        while (loop_dur := end - default_timer()) > 0.0:
+            with Events() as events:
+                event = events.get(timeout=loop_dur)
+                if isinstance(event, Events.Press):
+                    if event.key is Key.ctrl:
+                        return Status.fail_current
+                    elif event.key is Key.shift:
+                        return Status.fail_previous
+                    elif event.key is Key.esc:
+                        return Status.terminate
+    return Status.success
+
+
+def fail_previous(
+    forgotten: float,
+    phases: peekable,
+) -> None:
+    _CONTROLLER.tap(Key.left)
+    fail_current(forgotten=forgotten, phases=phases)
+
+
+def fail_current(forgotten: float, phases: peekable) -> None:
+    _CONTROLLER.tap("1")
+    _CONTROLLER.tap(Key.left)
+    tqdm_sleep(dur=forgotten, desc=TqdmDesc.confirm)
+    _CONTROLLER.tap(Key.right)
+    while phases.peek() is not Phase.test:
+        next(phases)
 
 
 if __name__ == "__main__":
