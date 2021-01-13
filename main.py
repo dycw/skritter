a4e4d0b02c646af3f52d5878931e05f30b01c824
Derek Wan <d.wan@icloud.com>
2021-01-13 12:23:38 +0800

    WIP

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+from collections import defaultdict
+from contextlib import suppress
 from enum import auto
 from enum import Enum
 from itertools import cycle
@@ -8,7 +10,7 @@ from logging import INFO
 from sys import stdout
 from time import sleep
 from timeit import default_timer
-from typing import Iterator
+from typing import Iterator, Optional, List, Callable, Dict, Iterable, Tuple
 
 from click import command
 from click import option
@@ -30,8 +32,9 @@ basicConfig(
 
 DEFAULT_INIT = 2.0
 DEFAULT_TEST = 1.5
-DEFAULT_REVIEW = 1.5
-DEFAULT_FORGOTTEN = 3.0
+DEFAULT_CHECK_ANSWER = 1.5
+DEFAULT_REVIEW_FORGOTTEN = 3.0
+DEFAULT_PAUSE = 1.
 
 
 _CONTROLLER = Controller()
@@ -39,12 +42,18 @@ _LOGGER = getLogger(__name__)
 _TQDM_STEP = 0.1
 
 
-class Forgotten(Enum):
+class Msg(Enum):
     current = auto()
     previous = auto()
+    pause = auto()
 
     def __str__(self) -> str:
-        return f"Marking {self.name} as forgotten..."
+        if self in {Msg.current, Msg.previous}:
+            return f"Marking {self.name} as forgotten..."
+        elif self is Msg.pause:
+            return 'Pausing...'
+        else:
+            raise ValueError(f"Invalid message: {self}")
 
 
 class Phase(Enum):
@@ -52,21 +61,114 @@ class Phase(Enum):
     review = auto()
 
 
-class Status(Enum):
-    success = auto()
-    fail_current = auto()
-    fail_previous = auto()
-    terminate = auto()
+class State(Enum):
+    initializing = auto()
+    testing = auto()
+    reviewing = auto()
+    confirming = auto()
+    pausing = auto()
+    shutting_down = auto()
 
 
-class TqdmDesc(Enum):
+    def __str__(self) -> str:
+        max_len = max(len(state) for state in State if state.name)
+        template = f"{{:{max_len}}}"
+        return template.format(self.name.title())
+
+
+
+
+
+def advance(
+        state: "State",
+        pre_pause:Optional["State"],
+        init :float,
+        test: float,
+        check_answer:float,
+        check_forgotten:float,
+        pause:float,
+)->Tuple["State", Optional["State"]]:
+    if state is State.initializing:
+        tqdm_sleep(dur=init, state=state)
+        return State.testing, pre_pause
+    elif state is State.testing:
+        if (outcome := get_outcome(dur=test, state=state)) is Outcome.test_and_check_success:
+            _CONTROLLER.tap("3")
+            _CONTROLLER.tap(Key.enter)
+            return State.reviewing, None
+        elif outcome is Outcome.test_review_fail_current:
+            _LOGGER.info(Msg.current)
+            _CONTROLLER.tap(Key.enter)
+            _CONTROLLER.tap("1")
+            return fail_previous(check_forgotten)
+        elif outcome is Outcome.test_review_fail_previous:
+            _LOGGER.info(Msg.previous)
+            return fail_previous(check_forgotten)
+        elif outcome is Outcome.test_review_pause:
+            _LOGGER.info(Msg.pause)
+            return State.pausing, state
+        elif outcome is Outcome.terminate:
+            _LOGGER.info('Shutting down...')
+            return State.shutting_down, None
+        else:
+            raise ValueError("Invalid outcome")
+    if  state is State.pausing:
+        key = get_key(keys={'`', Key.esc}, dur=pause, state=State.pausing)
+        if key is None:
+            return State.pausing, pre_pause
+        elif key == '`':
+            return pre_pause, None
+        elif key is Key.esc:
+            return State.shutting_down, None
+        else:
+            raise ValueError(f"Invalid key: {key}")
+    else:
+            phases = peekable(cycle(Phase))
+            for phase in phases:
+                if phase is Phase.test:
+                    status = get_status(dur=test, state=Desc.test)
+                elif phase is Phase.review:
+                    status = get_status(dur=check_answer, state=Desc.review)
+                else:
+                    raise ValueError(f"Invalid phase: {phase}")
+                if (phase is Phase.test) and (status is Status.test_and_check_success):
+                    _CONTROLLER.tap("3")
+                    _CONTROLLER.tap(Key.enter)
+                elif (phase is Phase.test) and (status is Status.test_review_fail_current):
+                    _LOGGER.info(str(Msg.current))
+                    _CONTROLLER.tap(Key.enter)
+                    _CONTROLLER.tap("1")
+                    fail_previous(check_forgotten, phases)
+                elif (phase is Phase.review) and (status is Status.test_and_check_success):
+                    _CONTROLLER.tap("3")
+                elif (phase is Phase.review) and (status is Status.test_review_fail_current):
+                    _LOGGER.info("Marking current as forgotten...")
+                    fail_current(forgotten=check_forgotten, phases=phases)
+                elif status is Status.test_review_fail_previous:
+                    _LOGGER.info(str(Msg.previous))
+                    fail_previous(forgotten=check_forgotten, phases=phases)
+                elif status is Status.terminate:
+                    _LOGGER.info("Terminating program...")
+                    return
+                else:
+                    raise ValueError(f"Invalid phase/status: {phase}, {status}")
+
+            if state is State.testing:
+                pass
+
+            else:
+                raise ValueError(f"Invalid state: {state}")
+
+
+class Desc(Enum):
     initial = auto()
     test = auto()
     review = auto()
     confirm = auto()
+    sleep =auto()
 
     def __str__(self) -> str:
-        max_len = max(len(desc.ing) for desc in TqdmDesc)
+        max_len = max(len(desc.ing) for desc in Desc)
         template = f"{{:{max_len}}}"
         return template.format(self.ing)
 
@@ -74,92 +176,113 @@ class TqdmDesc(Enum):
     def ing(self) -> str:
         return self.name.title() + "ing"
 
+class Outcome(Enum):
+    test_and_check_success = auto()
+    test_review_fail_current = auto()
+    test_review_fail_previous = auto()
+    test_review_pause = auto()
+    test_review_unpause = auto()
+
+    paused_continue = auto()
+    paused_to_unpaused = auto()
+    terminate = auto()
 
 @command()
 @option("--init", default=DEFAULT_INIT, type=float)
 @option("--test", default=DEFAULT_TEST, type=float)
-@option("--review", default=DEFAULT_REVIEW, type=float)
-@option("--forgotten", default=DEFAULT_FORGOTTEN, type=float)
+@option("--check-answer", default=DEFAULT_CHECK_ANSWER, type=float)
+@option("--review-forgotten", default=DEFAULT_REVIEW_FORGOTTEN, type=float)
+@option("--pause", default=DEFAULT_PAUSE, type=float)
 def main(
     *,
     init: float,
     test: float,
-    review: float,
-    forgotten: float,
+    check_answer: float,
+    review_forgotten: float,
+        pause: float
 ) -> None:
-    tqdm_sleep(dur=init, desc=TqdmDesc.initial)
-    phases = peekable(cycle(Phase))
-    for phase in phases:
-        if phase is Phase.test:
-            status = get_status(dur=test, desc=TqdmDesc.test)
-        elif phase is Phase.review:
-            status = get_status(dur=review, desc=TqdmDesc.review)
-        else:
-            raise ValueError(f"Invalid phase: {phase}")
-        if (phase is Phase.test) and (status is Status.success):
-            _CONTROLLER.tap("3")
-            _CONTROLLER.tap(Key.enter)
-        elif (phase is Phase.test) and (status is Status.fail_current):
-            _LOGGER.info(str(Forgotten.current))
-            _CONTROLLER.tap(Key.enter)
-            _CONTROLLER.tap("1")
-            fail_previous(forgotten, phases)
-        elif (phase is Phase.review) and (status is Status.success):
-            _CONTROLLER.tap("3")
-        elif (phase is Phase.review) and (status is Status.fail_current):
-            _LOGGER.info("Marking current as forgotten...")
-            fail_current(forgotten=forgotten, phases=phases)
-        elif status is Status.fail_previous:
-            _LOGGER.info(str(Forgotten.previous))
-            fail_previous(forgotten=forgotten, phases=phases)
-        elif status is Status.terminate:
-            _LOGGER.info("Terminating program...")
+    tqdm_sleep(dur=init, state=State.initializing)
+
+    state = State.initializing
+    pre_pause = None
+    while    True:
+        if state is State.shutting_down:
+            _LOGGER.info("Shutting down...")
             return
         else:
-            raise ValueError(f"Invalid phase/status: {phase}, {status}")
+            state, pre_pause = advance(state, pre_pause, init=init, test=test,
+                                       check_answer=check_answer,
+    review_forgotten=review_forgotten,
+                                       pause=pause)
 
-
-def tqdm_sleep(dur: float, desc: "TqdmDesc") -> None:
-    for _ in tqdm_dur(dur=dur, desc=desc):
+def tqdm_sleep(dur: float, state: "State") -> None:
+    for _ in tqdm_dur(dur=dur, state=state):
         sleep(_TQDM_STEP)
 
 
-def tqdm_dur(dur: float, desc: "TqdmDesc") -> Iterator[None]:
-    for _ in tqdm(range(int(dur / _TQDM_STEP)), desc=str(desc)):
+def tqdm_dur(dur: float, state: "State") -> Iterator[None]:
+    for _ in tqdm(range(int(dur / _TQDM_STEP)), desc=str(state)):
         yield None
 
 
-def get_status(dur: float, desc: "TqdmDesc") -> "Status":
-    for _ in tqdm_dur(dur=dur, desc=desc):
+def get_key(keys: Iterable[Key], dur: float, state: "State", ) ->Optional[Key]:
+    keys = set(keys)
+    for _ in tqdm_dur(dur=dur, state=state):
+        end = default_timer() + _TQDM_STEP
+        while (loop_dur := end - default_timer()) > 0.0:
+            with Events() as events:
+                event = events.get(timeout=loop_dur)
+                if isinstance(event, Events.Press) and ((key := event.key) in keys):
+                    return key
+    return None
+
+
+
+
+
+def get_outcome(dur:float, state: "State")->Outcome:
+    if state is State.initializing:
+        mapping = {'`': Outcome.paused_to_unpaused}
+        default = Outcome
+    if state in {State.testing, State.shutting_down}:
+        mapping = {
+            Key.ctrl: Outcome.test_review_fail_current,
+            Key.shift: Outcome.test_review_fail_previous,
+            '`': Outcome.test_review_pause,
+        }
+        default = Outcome.test_and_check_success
+    elif state is State.pausing:
+        mapping = {'`': Outcome.paused_to_unpaused}
+        default = Outcome.paused_continue
+    else:
+        raise ValueError(f'Invalid state: {state}')
+    for _ in tqdm_dur(dur=dur, state=state):
         end = default_timer() + _TQDM_STEP
         while (loop_dur := end - default_timer()) > 0.0:
             with Events() as events:
                 event = events.get(timeout=loop_dur)
                 if isinstance(event, Events.Press):
-                    if event.key is Key.ctrl:
-                        return Status.fail_current
-                    elif event.key is Key.shift:
-                        return Status.fail_previous
-                    elif event.key is Key.esc:
-                        return Status.terminate
-    return Status.success
+                    key = event.key
+                    try:
+                        return mapping[key]
+                    except KeyError:
+                        if key is Key.esc:
+                            return Outcome.terminate
+    return default
 
-
-def fail_previous(
-    forgotten: float,
-    phases: peekable,
-) -> None:
+def fail_previous(forgotten: float) -> Tuple["State",Optional['State']]:
     _CONTROLLER.tap(Key.left)
-    fail_current(forgotten=forgotten, phases=phases)
+    return fail_current(forgotten)
 
 
-def fail_current(forgotten: float, phases: peekable) -> None:
+def fail_current(forgotten: float) -> Tuple["State", Optional['State']]:
     _CONTROLLER.tap("1")
     _CONTROLLER.tap(Key.left)
-    tqdm_sleep(dur=forgotten, desc=TqdmDesc.confirm)
-    _CONTROLLER.tap(Key.right)
-    while phases.peek() is not Phase.test:
-        next(phases)
+    return State.confirming, None
+    # tqdm_sleep(dur=forgotten, state=Desc.confirm)
+    # _CONTROLLER.tap(Key.right)
+    # while phases.peek() is not Phase.test:
+    #     next(phases)
 
 
 if __name__ == "__main__":
